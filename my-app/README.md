dc234566642e6dc9df1618dedd32857439d3982e
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-05-14 11:39:00 -0400

    minimal bug example

---
+++ my-app/README.md
@@ -0,0 +1,1 @@
+This is a simple project used to report bug on svelte-kit.
---
+++ my-app/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script context="module" lang="ts">
+	import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+
+	export function load({ stuff }: LoadInput): LoadOutput {
+		return {
+			status: 302,
+			redirect: '/introduction/welcome'
+		};
+	}
+</script>
